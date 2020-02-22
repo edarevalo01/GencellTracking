413cac6fa0329e01475077b7611b85cc6c8f8e86
Alejandro Arevalo <edarevalo01@gmail.com>
2020-02-22 15:26:38 -0500

    Estructura básica de carpetas
    Tanto en el repositorio como dentro del proyecto frontend.

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { PersonaComponent } from './components/persona/persona.component';
+import { EpsComponent } from './components/eps/eps.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    PersonaComponent,
+    EpsComponent
   ],
   imports: [
     BrowserModule,
