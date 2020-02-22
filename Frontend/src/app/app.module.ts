65fd0786e038add71ef8acaa9d79a2b27949b0b0
Alejandro Arevalo <edarevalo01@gmail.com>
2020-02-22 15:39:05 -0500

    Integración PrimeNG
    Configuración de binding para ngModel y HttpClient.

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,24 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+// Native Components
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LoginComponent } from './components/login/login.component';
-import { PersonaComponent } from './components/persona/persona.component';
-import { EpsComponent } from './components/eps/eps.component';
+// Angular Components
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { LoginComponent } from "./components/login/login.component";
+import { PersonaComponent } from "./components/persona/persona.component";
+import { EpsComponent } from "./components/eps/eps.component";
+
+// Primeng Components
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    PersonaComponent,
-    EpsComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [AppComponent, LoginComponent, PersonaComponent, EpsComponent],
+	imports: [BrowserModule, BrowserAnimationsModule, FormsModule, HttpClientModule, AppRoutingModule],
+	providers: [],
+	bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
